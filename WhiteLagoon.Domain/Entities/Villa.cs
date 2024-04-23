506a8af86b323d05805083072089e0b7c1314809
Shahabs2004 <Shahabs2004@Gmail.com>
2024-04-23 09:44:48 +0330

    Some Range added to Villas Fields

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -15,13 +15,15 @@ namespace WhiteLagoon.Domain.Entities
         [Display(Name = "Villa Description")]
         public string? Description { get; set; }
         [Display(Name = "Price Per Night")]
+        [Range(0, 9999, ErrorMessage = "The Price Per Night must be between 0 and 9999.")]
         public double Price { get; set; }
         [Display(Name = "Square Feet")]
+        [Range(10,500,ErrorMessage = "Could not Be very low or high")]
         public int Sqft { get; set; }
         [Display(Name = "Number of Bedrooms")]
         public int Occupancy { get; set; }
         [Display(Name = "Image Url")]
-        public string? ImageUrl {get; set; }
+        public string? ImageUrl { get; set; }
         public DateTime? Created_Date { get; set; }
         public DateTime? Update_Date { get; set; }
 
