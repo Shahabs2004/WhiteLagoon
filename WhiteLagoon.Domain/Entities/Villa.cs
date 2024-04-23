73e9182574d43a3b48edb52f1f86063304478782
Shahabs2004 <Shahabs2004@Gmail.com>
2024-04-23 09:40:52 +0330

    Create Villa Form  designed Villas Fields annotation Added

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,17 @@ namespace WhiteLagoon.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
+        [Display(Name = "Villa Name")]
         public required string Name { get; set; }
+        [Display(Name = "Villa Description")]
         public string? Description { get; set; }
+        [Display(Name = "Price Per Night")]
         public double Price { get; set; }
+        [Display(Name = "Square Feet")]
         public int Sqft { get; set; }
+        [Display(Name = "Number of Bedrooms")]
         public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
         public string? ImageUrl {get; set; }
         public DateTime? Created_Date { get; set; }
         public DateTime? Update_Date { get; set; }
--- WhiteLagoon.Inrfastructure/Migrations/20240423060958_DataAnotationOnVillaFields.Designer.cs
+++ WhiteLagoon.Inrfastructure/Migrations/20240423060958_DataAnotationOnVillaFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WhiteLagoon.Inrfastructure.Data;
 
@@ -11,9 +12,11 @@ using WhiteLagoon.Inrfastructure.Data;
 namespace WhiteLagoon.Inrfastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240423060958_DataAnotationOnVillaFields")]
+    partial class DataAnotationOnVillaFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
