b6cae8c20e4e6020601c913d13468f9138b3b598
Shahabs2004 <Shahabs2004@Gmail.com>
2024-04-27 09:38:08 +0330

    Added Update Villas tasks and Changed 404 not Found Page

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -10,22 +10,33 @@ namespace WhiteLagoon.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
+
         [Display(Name = "Villa Name")]
+        [Required(ErrorMessage = "The Villa Name is required.")]
+        [StringLength(100, ErrorMessage = "The Villa Name cannot exceed 100 characters.")]
+        [MinLength(5)]
         public required string Name { get; set; }
+
         [Display(Name = "Villa Description")]
         public string? Description { get; set; }
+
         [Display(Name = "Price Per Night")]
         [Range(0, 9999, ErrorMessage = "The Price Per Night must be between 0 and 9999.")]
         public double Price { get; set; }
+
         [Display(Name = "Square Feet")]
-        [Range(10,500,ErrorMessage = "Could not Be very low or high")]
+        [Range(10, 500, ErrorMessage = "The Square Feet must be between 10 and 500.")]
         public int Sqft { get; set; }
+
         [Display(Name = "Number of Bedrooms")]
+        [Range(1, 10, ErrorMessage = "The Number of Bedrooms must be between 1 and 10.")]
         public int Occupancy { get; set; }
+
         [Display(Name = "Image Url")]
+        [Url(ErrorMessage = "The Image Url is not a valid URL.")]
         public string? ImageUrl { get; set; }
+
         public DateTime? Created_Date { get; set; }
         public DateTime? Update_Date { get; set; }
-
     }
 }
--- whiteLagoon.Web/Controllers/VillaController.cs
+++ whiteLagoon.Web/Controllers/VillaController.cs
@@ -1,23 +1,81 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Inrfastructure.Data;
 
 namespace whiteLagoon.Web.Controllers
 {
+    // Defining the VillaController class which inherits from the Controller class
     public class VillaController : Controller
     {
+        // Declaring a private readonly field of type ApplicationDbContext
         private readonly ApplicationDbContext _db;
 
+        // Defining the constructor for the VillaController class
         public VillaController(ApplicationDbContext db)
         {
-            _db = db;
+            _db = db; // Assigning the passed in db value to the _db field
         }
+
+        // Defining the Index action method
         public IActionResult Index()
         {
-            var villas = _db.Villas.ToList();
-            return View(villas);
+            var villas = _db.Villas.ToList(); // Getting all villas from the database and converting to a list
+            return View(villas); // Returning the view with the list of villas
         }
+
+        // Defining the Create action method
         public IActionResult Create()
         {
+            return View(); // Returning the view
+        }
+
+        // Defining the Create action method that responds to HTTP POST requests
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            // Checking if the name and description of the villa are the same
+            if (obj.Name == obj.Description)
+            {
+                // Adding an error to the ModelState
+                ModelState.AddModelError("description", "the Description cannot be Exactly match the Name.");
+            }
+            // Checking if the ModelState is valid
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj); // Adding the villa to the database
+                _db.SaveChanges(); // Saving the changes to the database
+                return RedirectToAction("Index"); // Redirecting to the Index action method
+            }
+            else
+            {
+                return View(obj); // Returning the view with the villa object
+            }
+        }
+
+        // Defining the Update action method
+        public IActionResult Update(int villaId)
+        {
+            // Getting the villa with the specified id from the database
+            Villa? obj = _db.Villas.FirstOrDefault((u => u.Id == villaId));
+            // Checking if the villa is null
+            if (obj == null)
+            {
+                return RedirectToAction("Error","Home"); // Returning a NotFound result
+            }
+
+            return View(obj); // Returning the view with the villa object
+        }
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            // Checking if the ModelState is valid
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Villas.Update(obj); // Adding the villa to the database
+                _db.SaveChanges(); // Saving the changes to the database
+                return RedirectToAction("Index"); // Redirecting to the Index action method
+            }
+
             return View();
         }
     }
