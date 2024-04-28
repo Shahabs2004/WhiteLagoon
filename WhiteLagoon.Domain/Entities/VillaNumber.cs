641b97d9ee64d4b7412d6a9b329aa5a9de389b94
Shahabs2004 <Shahabs2004@Gmail.com>
2024-04-28 12:45:37 +0330

    Fixed bug of adding Villa Model State problem

--- WhiteLagoon.Domain/Entities/VillaNumber.cs
+++ WhiteLagoon.Domain/Entities/VillaNumber.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -12,13 +13,16 @@ namespace WhiteLagoon.Domain.Entities
     {
         // Represents the primary key of the VillaNumber entity
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Villa Number")]
         public int Villa_Number { get; set; }
 
         // Represents the foreign key to the Villa entity
         [ForeignKey("Villa")]
+        [Display(Name = "Villa Id")]
         public int VillaId { get; set; }
 
         // Represents the navigation property to the Villa entity
+        [ValidateNever]
         public Villa Villa { get; set; }
 
         // Represents the special details of the VillaNumber
---
+++ whiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Inrfastructure.Data;
+
+namespace whiteLagoon.Web.Controllers
+{
+    // Defining the VillaController class which inherits from the Controller class
+    public class VillaNumberController : Controller
+    {
+        // Declaring a private readonly field of type ApplicationDbContext
+        private readonly ApplicationDbContext _db;
+
+        // Defining the constructor for the VillaController class
+        public VillaNumberController(ApplicationDbContext db)
+        {
+            _db = db; // Assigning the passed in db value to the _db field
+        }
+
+        // Defining the Index action method
+        public IActionResult Index()
+        {
+            var villasNumbers = _db.VillaNumbers.ToList(); // Getting all villas from the database and converting to a list
+            return View(villasNumbers); // Returning the view with the list of villas
+        }
+
+        // Defining the Create action method
+        public IActionResult Create()
+        {
+            return View(); // Returning the view
+        }
+
+        // Defining the Create action method that responds to HTTP POST requests
+        [HttpPost]
+        public IActionResult Create(VillaNumber obj)
+        {
+            //ModelState.Remove("Villa"); Commented out Due to Added Annotation to Property
+            // Checking if the ModelState is valid
+            if (ModelState.IsValid)
+            {
+                _db.VillaNumbers.Add(obj); // Adding the villa to the database
+                _db.SaveChanges(); // Saving the changes to the database
+                TempData["Success"] = "Villa number Created Successfully";
+                return RedirectToAction("Index"); // Redirecting to the Index action method
+            }
+            else
+            {
+                TempData["Error"] = "Error occurred while creating the villa number ";
+                return View(obj); // Returning the view with the villa object
+            }
+        }
+
+        // Defining the Update action method
+        public IActionResult Update(int villaId)
+        {
+            // Getting the villa with the specified id from the database
+            Villa? obj = _db.Villas.FirstOrDefault((u => u.Id == villaId));
+            // Checking if the villa is null
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home"); // Returning a NotFound result
+            }
+
+            return View(obj); // Returning the view with the villa object
+        }
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            // Checking if the ModelState is valid
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Villas.Update(obj); // Adding the villa to the database
+                _db.SaveChanges(); // Saving the changes to the database
+                TempData["Success"] = "Villa Updated Successfully";
+                return RedirectToAction("Index"); // Redirecting to the Index action method
+            }
+
+            TempData["Error"] = "Error occurred while updating the villa";
+            return View();
+        }
+
+        // Defining the Delete action method
+        public IActionResult Delete(int villaId)
+        {
+            // Getting the villa with the specified id from the database
+            Villa? obj = _db.Villas.FirstOrDefault((u => u.Id == villaId));
+            // Checking if the villa is null
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home"); // Returning a NotFound result
+            }
+
+            return View(obj); // Returning the view with the villa object
+        }
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault((u => u.Id == obj.Id));
+            // Checking if the ModelState is valid
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb); // Remove the villa from the database
+                _db.SaveChanges(); // Saving the changes to the database
+                TempData["Success"] = "Villa Deleted Successfully";
+                return RedirectToAction("Index"); // Redirecting to the Index action method
+            }
+
+            TempData["Error"] = "Error occurred while deleting the villa";
+            return View();
+        }
+    }
+}
