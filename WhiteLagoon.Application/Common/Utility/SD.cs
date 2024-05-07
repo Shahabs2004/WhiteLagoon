96812e630874139b3c6ccb108372ccf4a9cb3482
Shahabs2004 <Shahabs2004@Gmail.com>
2024-05-07 12:37:21 +0330

    Implemented Register/Login/logout Functionality

---
+++ WhiteLagoon.Application/Common/Utility/SD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Application.Common.Utility
+{
+    //SD Stands for Static Details
+    public static class SD
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_User = "User";
+        public const string Role_Employee = "Employee";
+        public const string Role_Customer = "Customer";
+    }
+}
